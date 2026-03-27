c234b5ffacd958a4f4d17cbdc9a5c043df78f2ef
pauliAlonso <paulialonso33@gmail.com>
2026-03-27 14:09:47 -0300

    Add files via upload

--- exercise_leap_year.py
+++ exercise_leap_year.py
@@ -1,20 +1,9 @@
 def leap_year():
-    """
-    Ejercicio 11 (Desafío) - Año Bisiesto
+ 
+    año = int(input())
 
-    Leer un año mediante input(). Determinar si es un año bisiesto e imprimir el resultado.
-    Un año es bisiesto si:
-    - Es divisible por 4, Y
-    - NO es divisible por 100, O es divisible por 400
+    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+        print(f"El año {año} es bisiesto")
+    else:
+        print(f"El año {año} no es bisiesto")
 
-    Ejemplo:
-        Para la entrada "2000", la salida esperada es:
-        El año 2000 es bisiesto
-
-        Para la entrada "2001", la salida esperada es:
-        El año 2001 no es bisiesto
-
-        Para la entrada "1700", la salida esperada es:
-        El año 1700 no es bisiesto
-    """
-    pass
